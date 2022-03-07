78dd47ede7a35cab3bd036c61d39c39113a80c81
ketul.mistry <ketul.mistry@DT035>
2022-03-07 17:55:57 +0530

    Add project files.

---
+++ ThemeSet/Controllers/ProtfolioController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThemeSet.Controllers
+{
+    public class ProtfolioController : Controller
+    {
+        // GET: Protfolio
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Homes()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Protfolio()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Readmore()
+        {
+            return View();
+        }
+    }
+}
---
+++ ThemeSet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThemeSet.MvcApplication" Language="C#" %>
